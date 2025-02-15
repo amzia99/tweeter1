2e29ba477c52973d7256030d8664ba6ae31e691d
Abdullah Zia <amz1cflp99@gmail.com>
2025-02-14 20:24:53 -0500

    realtime character count added

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,15 @@
 // character count code
 $(document).ready(function() {
-  console.log("Document is ready. Character counter script is running.");
+  $(".new-tweet textarea").on("input", function() {
+    let maxLength = 140;
+    let currentLength = $(this).val().length;
+    let remainingChars = maxLength - currentLength;
+
+    let counterElement = $(this).closest(".new-tweet").find(".counter");
+    counterElement.text(remainingChars);
+
+    // CSS styling class
+    counterElement.toggleClass("invalid", remainingChars < 0);
+  });
 });
 
