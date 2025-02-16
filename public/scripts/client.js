84422be2d8b284bad6444ff74293dbf0a51bf598
Abdullah Zia <amz1cflp99@gmail.com>
2025-02-15 21:15:18 -0500

    added client side code in client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,31 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// client side code
+const createTweetElement = function(tweet) {
+  const { name, avatars, handle } = tweet.user;
+  const { text } = tweet.content;
+  const timeAgo = timeago.format(tweet.created_at); // Use timeago.js to format the timestamp
+
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="user-info">
+          <img src="${avatars}" alt="User Avatar">
+          <h3>${name}</h3>
+        </div>
+        <span class="handle">${handle}</span>
+      </header>
+      <p class="tweet-content">${text}</p>
+      <footer>
+        <span class="timestamp">${timeAgo}</span>
+        <div class="tweet-actions">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+
+  return $tweet;
+};
+
 
